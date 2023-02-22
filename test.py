90df020899a8ff82b9a8665376d8d976917b6c8c
Minina <milena.alfvaq@gmail.com>
2023-02-22 13:32:02 -0500

    removed files that should be ignored

--- test.py
+++
@@ -1,9 +0,0 @@
-from flight_search import FlightSearch
-from notification_manager import NotificationManager
-
-flight_search = FlightSearch()
-notification_manager = NotificationManager()
-
-search = flight_search.search_price("BUE", 400)
-print(search)
-# notification_manager.send_email(search)
